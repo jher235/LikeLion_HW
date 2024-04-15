6ec4149fe1bd250e10ae9c3887d31604a2891c96
jher235 <tim668666@gmail.com>
2024-04-15 22:29:17 +0900

    first commit

---
+++ src/main/java/org/mjulikelion/liikelion12thhw/Liikelion12thHwApplication.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.liikelion12thhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Liikelion12thHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Liikelion12thHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mjulikelion/liikelion12thhw/Liikelion12thHwApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.liikelion12thhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Liikelion12thHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
