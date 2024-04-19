c3905860dfeb76176057972e0f4369bf2415806b
jher235 <tim668666@gmail.com>
2024-04-19 20:31:35 +0900

    first commit

--- src/main/java/org/mjulikelion/liikelion12thhw/Likelion12thHwApplication.java
+++ src/main/java/org/mjulikelion/liikelion12thhw/Likelion12thHwApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Liikelion12thHwApplication {
+public class Likelion12thHwApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Liikelion12thHwApplication.class, args);
+		SpringApplication.run(Likelion12thHwApplication.class, args);
 	}
 
 }
